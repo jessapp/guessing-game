73896e6c545385688c62ea494a78b9221d0a05fe
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-03 12:27:37 -0800

    Random Number Generated

--- game.py
+++ game.py
@@ -1,4 +1,18 @@
 # Put your code here
+from random import randint
+
 print "Hello! What's your name?"
 
 name = raw_input("> ")
+
+number = randint(1, 100)
+
+print "%s, I'm thinking of a number between 1 and 100. \nTry to guess my number." % (name)
+
+guess = raw_input("> ")
+
+num_guesses = 0
+
+guessed = False
+
+#while guessed == False:
