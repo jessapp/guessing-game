eaf4d2e8d885524142200dd8e7b598d1afc66c32
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-03 15:22:44 -0800

    Added play again option

--- game.py
+++ game.py
@@ -5,31 +5,40 @@ print "Hello! What's your name?"
 
 name = raw_input("> ")
 
-number = randint(1, 100)
+play_again = True
 
-print "%s, I'm thinking of a number between 1 and 100. \nTry to guess my number." % (name)
+while play_again == True:
+    number = randint(1, 100)
 
+    print "%s, I'm thinking of a number between 1 and 100. \nTry to guess my number." % (name)
 
-num_guesses = 0
+    num_guesses = 0
 
-guessed = False
+    guessed = False
 
-while not guessed:
-    try:
-        guess = int(raw_input(">  "))
-        break
-    except ValueError:
-        print "That's Not even a number! Try Again!"
+    while not guessed:
+        try:
+            guess = int(raw_input(">  "))
+            num_guesses += 1
+            if guess == number:
+                print "Congratulations! You guessed the number in %s guesses!" % (num_guesses)
+                guessed = True
+                print "Would you like to play again?"
+                play_again = raw_input("Y or N: ")
+                if play_again == "Y":
+                    play_again = True
+                elif play_again == "N":
+                    play_again = False
+                else:
+                    print "That's not a valid answer! Try again."
+            elif guess < 1 or guess > 100:
+                print "Can't you read?! Guess again, in the range!"
+            elif guess < number:
+                print "Too Low!"
+            elif guess > number:
+                print "Too High!"
+            else:
+                print "Sorry, I don't understand that input!"
+        except ValueError:
+            print "That's Not even a number! Try Again!"
 
-    num_guesses += 1
-    if guess == number:
-        print "Congratulations! You guessed the number in %s guesses!" % (num_guesses)
-        guessed = True
-    elif guess < 1 or guess > 100:
-        print "Can't you read?! Guess again, in the range!"
-    elif guess < number:
-        print "Too Low!"
-    elif guess > number:
-        print "Too High!"
-    else:
-        print "Sorry, I don't understand that input!"
